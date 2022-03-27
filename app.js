4faf51a119f17f538b9c51ec2ab1baa8ffba2151
evgeniimx <mixalev10a@gmail.com>
2022-03-28 00:36:15 +0300

    1

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     const btn_input_person = document.querySelector('.add_input_person');
+    let inputs_p = document.querySelectorAll(".person_input_list input");
     // добавляем поля для персон
+
     btn_input_person.addEventListener("click", e => {
         let person_inputs = document.querySelectorAll(".person_input_list input");
         let count_inputs = person_inputs.length;
         let input = `<span  class="data_person_input" data-person="p${count_inputs+1}">
-                        <input id="p${count_inputs+1}" type="text" name="person" required>
+                        <input id="p${count_inputs+1}" type="text" name="person" required readonly>
                         <button type="button" class="add_person_area"
                                 data-area="a${count_inputs+1}"
                                 data-person="p${count_inputs+1}">
@@ -16,47 +18,78 @@ document.addEventListener("DOMContentLoaded", function(event) {
     });
 
 
+
+
+
     // Зона фотографии
+    let form_persone = document.querySelector("form");
     let area = document.querySelector('.area');
     let area_click = document.querySelector('.area_click');
     let divRect = area.getBoundingClientRect();
 
+    let all_person = area_click.querySelectorAll('.area_person');
 
 
-
+// клик по фото
     area_click.addEventListener("click", e => {
         let relX = e.pageX - divRect.left-25;
         let relY = e.pageY - divRect.top-25;
         let location = `top: ${relY}px; left: ${relX}px`;
-
-            $('.area_person').draggable();
+        let data_location = JSON.stringify({'y': relY, 'x':relX});
 
         let id_person = area_click.id;
         if ( !id_person ) return;
-        let border_person = document.createElement('div');
-        border_person.classList.add('area_person'),
-        border_person.setAttribute('data-pesron', id_person);
-        border_person.style.cssText = location;
+        let text_person = document.createElement('span');
+        let box_person = `<div class='area_person' data-person='${id_person}' style='${location}'>
+                            <span class='area_person__text'></span>
+                        </div>`;
 
-        if ( area_click.querySelector(`[data-pesron=${id_person}]`) ) {
-            area_click.querySelector(`[data-pesron=${id_person}]`).style.cssText = location;
+        if ( area_click.querySelector(`[data-person=${id_person}]`)  ) {
+            area_click.querySelector(`[data-person=${id_person}`).style.cssText = location;
+            form_persone.querySelector(`input#${id_person}]`).setAttribute('data-location', data_location)
             return;
         } else {
-            area_click.append(border_person);
+            form_persone.querySelector(`input#${id_person}`).setAttribute('data-location', data_location)
+            area_click.innerHTML += box_person;
+
+            form_persone.querySelector(`input#${id_person}`).style.cssText = 'cursor: text';
+            form_persone.querySelector(`input#${id_person}`).removeAttribute('readonly');
+
+
         }
 
     });
 
-    let form_persone = document.querySelector("form");
+    // let input = document.querySelector(`.input#${e.target.id}`);
+    // if ( !input.dataset.location || input.dataset.location == '' ) return;
+    // input.style.cssText = 'cursor: text';
+    // form_persone.querySelector(`input#${btn.dataset.person}`).removeAttribute('readonly');
+
+
+    // ввод имени персонажа
+    inputs_p.forEach( inp => {
+        inp.addEventListener('input', e => {
+            let span = document.querySelector(`.area_person[data-person="${e.target.id}"] span`);
+            if ( span ) {
+                span.textContent = e.target.value;
+            }
+        });
+    });
 
     form_persone.addEventListener('click', form => {
         if ( form.target.classList.contains('add_person_area') ) {
             let btn = form.target;
             if ( btn.classList.contains('edit') ) {
                 clean_activ_btn();
+                console.log(btn.dataset.person);
                 area_click.id = '';
             } else {
                 clean_activ_btn();
+                console.log(btn.dataset.person);
+                form_persone.querySelector(`input#${btn.dataset.person}`).style.cssText = 'cursor: text';
+                form_persone.querySelector(`input#${btn.dataset.person}`).removeAttribute('readonly');
+
+
                 btn.classList.add('edit')
                 area_click.id = btn.dataset.person;
             }
@@ -66,19 +99,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
         let data_person_input = form_persone.querySelectorAll('.add_person_area');
         data_person_input.forEach( btns => {
             btns.classList.remove('edit');
+            document.querySelector(`input#${btns.dataset.person}`).setAttribute('readonly','readonly');
+            document.querySelector(`input#${btns.dataset.person}`).style.cssText = 'cursor: not-allowed';
         });
     }
-
-
-
-    // area.addEventListener('mousemove', function(e){
-    //     let relX = e.pageX - divRect.left;
-    //     let relY = e.pageY - divRect.top;
-    //     area.textContent = `
-    //         relX: ${ relX }, absX: ${ e.pageX }
-    //         relY: ${ relY }, absY: ${ e.pageY }
-    //     `;
-    // });
 
     window.addEventListener('resize', function(){
         divRect = area.getBoundingClientRect();
